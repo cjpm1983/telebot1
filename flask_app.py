a783f463a003896045759c027fe97c08ee0a8b16
cjpm1983 <cjpm1983@gmail.com>
2021-12-24 08:58:42 -0500

    Refactorizado con nuevo ejemploque incluye handlers en Flask
    hecho en laptop para subir y probar

--- flask_app.py
+++ flask_app.py
@@ -1,83 +1,44 @@
-
-# A very simple Flask Hello World app for you to get started with...
-#con handlers
 from flask import Flask, request
-
+import os
+import logging
 import telegram
+import time
+from telegram import Update
+from telegram.ext import CallbackContext, CommandHandler, Updater
 
-
+#locales
 from telebot.credentials import bot_token, bot_user_name,URL
 from telebot.mastermind import get_response
-
-global bot
+#global bot
 global TOKEN
 TOKEN = bot_token
-bot = telegram.Bot(token=TOKEN)
+#bot = telegram.Bot(token=TOKEN)
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
-
-#from telegram.ext import (Updater, CommandHandler)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger("test")
+#herokuname = "name"
+#PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN, use_context=True)
+bot = updater.bot
+update_queue = updater.update_queue
+dispatcher = updater.dispatcher
 
 app = Flask(__name__)
 
-#@app.route('/')
-#def hello_world():
-#    return 'Hello from Flask!'
-
-@app.route('/{}'.format(TOKEN), methods=['POST'])
-def respond():
-
-    updater = Updater(TOKEN)
-
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    echo_handler = MessageHandler(Filters.all, echo)
-    updater.dispatcher.add_handler(echo_handler)
-    
-    # retrieve the message in JSON and then transform it to Telegram object
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
-# get the chat_id to be able to respond to the same user
-    #chat_id = update.message.chat.id
-    
-# get the message id to be able to reply to this specific message
-    #msg_id = update.message.message_id
-# Telegram understands UTF-8, so encode text for unicode compatibility
-    #text = update.message.text.encode('utf-8').decode()
-    #print("got text message :", text)
-# here we call our super AI
-
-    '''    
-    bundle = get_response(text)
-
-    response = bundle["txt"]
-    if len(response) > 4096:
-       for x in range(0, len(response), 4096):
-          bot.send_message(chat_id=chat_id, text=response[x:x+4096])
-    else:
-       bot.send_message(chat_id=chat_id, text=response)
-
-    if "btns" in bundle.keys():
-       btns = bundle["btns"]
-       #bot.sendMessage (chat_id=chat_id, text=str("---"), reply_markup=btns)
-    '''
-    return 'ok'
+@app.route('/' + TOKEN, methods=['POST'])
+def webhook():
+    update = telegram.update.Update.de_json(request.get_json(force=True), bot)
+    logger.info(f"update id is : {update.update_id}")
+    update_queue.put(update)
+    return 'OK'
 
 
-def button(update: telegram.Update, context: CallbackContext) -> None:
-    """Parses the CallbackQuery and updates the message text."""
-    query = update.callback_query
-
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    query.answer()
-
-    query.edit_message_text(text=f"Selected option: {query.data}")
-
-def echo(update, context):
-   context.bot.send_message(chat_id = update.effective_chat.id, text = update.message.text)
+def start(update:Update, context:CallbackContext):
+    update.message.reply_text("ok")
 
 
-
+#eSTE ultimo ejemplo no incluye esta funcionsino que llama setwebhook 
+#en elmain, lodejamos pues no debe de dar conflicto
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     # we use the bot object to link the bot to our app which live
@@ -90,11 +51,23 @@ def set_webhook():
         return "webhook setup failed"
 
 
+
 @app.route('/')
 def index():
     return '.'
 
+
 if __name__ == '__main__':
+    dispatcher.add_handler(CommandHandler("start", start))
+    updater._init_thread(dispatcher.start, "dispatcher")
+    
+    #teniamos bot.setWebhook pero cambiamos a set_webhook porque los bot se 
+    #declaran diferente en este ejemplo, aqui es a partir del updater en el otro es de telegram
+    bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    #bot.set_webhook(f"https://{herokuname}.herokuapp.com/{TOKEN}")
+
+    time.sleep(5)
     # note the threaded arg which allow
     # your app to have more than one thread
     app.run(threaded=True)
+    
